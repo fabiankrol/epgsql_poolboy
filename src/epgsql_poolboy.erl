44a66b8b0be8ca72bfe2e091ff212cf7b483bfcf
Aaron France <aaron.l.france@gmail.com>
2014-11-14 19:56:39 +0100

    Basic equery working

--- src/epgsql_poolboy.erl
+++ src/epgsql_poolboy.erl
@@ -1,5 +1,7 @@
 -module(epgsql_poolboy).
 
+
+-export([equery/2]).
 -export([stop_pool/1]).
 -export([start_pool/3]).
 
@@ -17,3 +19,26 @@ stop_pool(atom()) ->
     ok | stop_return().
 stop_pool(Name) ->
     epgsql_poolboy_sup:stop_pool(Name).
+
+equery(PoolName, Sql) ->
+    exec(PoolName, {equery, Sql}).
+
+exec(PoolName, Args) ->
+   PoolNameBin = atom_to_binary(PoolName, latin1),
+   Name = list_to_binary([<<"epgsql_poolboy.">>, PoolNameBin]),
+    %%TODO: Steal puzza's fancy pants metric on the arguments: stat(Args)])
+    Fun = fun(Worker) ->
+                  Metric = folsom_metrics:histogram_timed_begin(Name),
+                  Res = gen_server:call(Worker, Args),
+                  ok = histogram_timed_notify(Metric),
+                  Res
+          end,
+    poolboy:transaction(PoolName, Fun).
+
+histogram_timed_notify({Name, _} = Metric) ->
+     try
+         ok = folsom_metrics:histogram_timed_notify(Metric)
+     catch _:_ ->
+             folsom_metrics:new_histogram(Name),
+             folsom_metrics:safely_histogram_timed_notify(Metric)
+     end.
--- src/epgsql_poolboy_worker.erl
+++ src/epgsql_poolboy_worker.erl
@@ -1,9 +1,16 @@
 -module(epgsql_poolboy_worker).
 
+
+-export([code_change/3]).
+-export([handle_call/3]).
+-export([handle_cast/2]).
+-export([handle_info/2]).
 -export([init/1]).
 -export([start_link/1]).
+-export([terminate/2]).
 
 -behaviour(poolboy_worker).
+-behaviour(gen_server).
 
 -record(state, {conn :: pid()}).
 
@@ -11,5 +18,52 @@
 start_link(Args) ->
     gen_server:start_link(?MODULE, Args, []).
 
-init(_) ->
-    {ok, #state{}}.
+connect(Host, undefined, Opts) ->
+    pgsql:connect(Host, Opts);
+connect(Host, Password, Opts) ->
+    pgsql:connect(Host, Password, Opts).
+
+init(Args) ->
+    Host = proplists:get_value(host, Args, "localhost"),
+    Password = proplists:get_value(password, Args),
+    ConnOpts = proplists:get_value(opts, Args, []),
+    {ok, Pid} = connect(Host, Password, ConnOpts),
+    {ok, #state{conn=Pid}}.
+
+
+handle_call({F, A1, A2, A3, A4, A5, A6}, _From, State=#state{conn=Conn}) ->
+    Reply = pgsql:F(Conn, A1, A2, A3, A4, A5, A6),
+    handle_call_reply(Reply, State);
+handle_call({F, A1, A2, A3, A4, A5}, _From, State=#state{conn=Conn}) ->
+    Reply = pgsql:F(Conn, A1, A2, A3, A4, A5),
+    handle_call_reply(Reply, State);
+handle_call({F, A1, A2, A3, A4}, _From, State=#state{conn=Conn}) ->
+    Reply = pgsql:F(Conn, A1, A2, A3, A4),
+    handle_call_reply(Reply, State);
+handle_call({F, A1, A2, A3}, _From, State=#state{conn=Conn}) ->
+    Reply = pgsql:F(Conn, A1, A2, A3),
+    handle_call_reply(Reply, State);
+handle_call({F, A1, A2}, _From, State=#state{conn=Conn}) ->
+    Reply = pgsql:F(Conn, A1, A2),
+    handle_call_reply(Reply, State);
+handle_call({F, A1}, _From, State=#state{conn=Conn}) ->
+    Reply = pgsql:F(Conn, A1),
+    handle_call_reply(Reply, State);
+handle_call(F, _From, State=#state{conn=Conn}) ->
+    Reply = pgsql:F(Conn),
+    handle_call_reply(Reply, State).
+
+handle_cast(_Msg, State) ->
+    {stop, unhandled_cast, State}.
+
+terminate(_Reason, #state{conn=Conn}) ->
+    ok = pgsql:close(Conn).
+
+handle_call_reply(Reply, State) ->
+    {reply, Reply, State}.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+handle_info(_, State) ->
+    {noreply, State}.
